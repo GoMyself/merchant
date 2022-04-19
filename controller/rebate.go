f14fcc6b3f57c9f8f3f55ef83f3dbf8aae0e2567
brandon <brandon@ym.com>
2022-04-19 17:31:36 +0700

    fix

--- controller/rebate.go
+++ controller/rebate.go
@@ -13,12 +13,13 @@ func (that *RebateController) Scale(ctx *fasthttp.RequestCtx) {
 
 	vs := model.RebateScale()
 	s := fmt.Sprintf(
-		`{"ty":"%s","zr":"%s","dj":"%s","qp":"%s","dz":"%s"}`,
+		`{"ty":"%s","zr":"%s","dj":"%s","qp":"%s","dz":"%s","cp":"%s"}`,
 		vs.TY.StringFixed(1),
 		vs.ZR.StringFixed(1),
 		vs.DJ.StringFixed(1),
 		vs.QP.StringFixed(1),
 		vs.DZ.StringFixed(1),
+		vs.CP.StringFixed(1),
 	)
 
 	helper.PrintJson(ctx, true, s)
