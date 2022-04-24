306fc0a46d0fb626d550f5d8c55a8f8f84f25000
bran <bran@git.com>
2022-04-24 04:14:45 -0700

    mod sh

--- cg.sh
+++ cg.sh
@@ -12,5 +12,4 @@ BuildGoVersion=`go version`
 
 go build -ldflags "-X main.gitReversion=${GitReversion}  -X 'main.buildTime=${BuildTime}' -X 'main.buildGoVersion=${BuildGoVersion}'" -o $PROJECT
 
-scp -i /home/gocloud-yiy-rich $PROJECT p3test@34.92.240.177:/home/centos/workspace/cg/merchant/merchant_cg
-ssh -i /home/gocloud-yiy-rich p3test@34.92.240.177 "sh /home/centos/workspace/cg/merchant/cg.sh"
+scp -i /opt/data/p3test -P 10087 $PROJECT p3test@34.92.240.177:/home/centos/workspace/cg/$PROJECT
