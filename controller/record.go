4012b42c544fc4e65d9ef9fc440f995a68e5338b
raiden <solor13688@gmail.com>
2022-05-10 21:14:01 +0700

    佣金钱包账变记录

--- controller/record.go
+++ controller/record.go
@@ -159,13 +159,13 @@ func (that *RecordController) Transaction(ctx *fasthttp.RequestCtx) {
 		// 佣金钱包账变
 		if types != "" {
 			cashTypes := strings.Split(types, ",")
-			for _, v := range cashTypes {
-				v, err := strconv.Atoi(v)
-				if err != nil || v < model.COTransactionReceive || v > model.COTransactionRation {
-					helper.Print(ctx, false, helper.CashTypeErr)
-					return
-				}
-			}
+			//for _, v := range cashTypes {
+			//	v, err := strconv.Atoi(v)
+			//	if err != nil || v < model.COTransactionReceive || v > model.COTransactionRation {
+			//		helper.Print(ctx, false, helper.CashTypeErr)
+			//		return
+			//	}
+			//}
 
 			if len(cashTypes) > 0 {
 				ex["cash_type"] = types
