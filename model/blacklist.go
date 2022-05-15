af40f699ad23cf7a16e607a45b9a66443f80fb84
Linch <Linch@gmail.com>
2022-05-15 13:53:18 +0800

    xg

--- model/blacklist.go
+++ model/blacklist.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"merchant2/contrib/helper"
-	"strconv"
 	"strings"
 
 	g "github.com/doug-martin/goqu/v9"
@@ -60,26 +59,6 @@ func BlacklistList(page, pageSize uint, startTime, endTime string, ty int, ex g.
 	err := meta.MerchantDB.Select(&data.D, query)
 	if err != nil {
 		return data, pushLog(fmt.Errorf("%s,[%s]", err.Error(), query), helper.DBErr)
-	}
-
-	if ty == TyBankcard {
-
-		var (
-			ids []string
-		)
-		for _, v := range data.D {
-			ids = append(ids, v.ID)
-		}
-
-		d, err := grpc_t.DecryptAll(ids, true, []string{"bankcard"})
-		if err != nil {
-			fmt.Println("grpc_t.DecryptAll err = ", err)
-			return data, errors.New(helper.GetRPCErr)
-		}
-
-		for k, v := range data.D {
-			data.D[k].Value = d[v.ID]["bankcard"]
-		}
 	}
 
 	return data, nil
@@ -149,6 +128,7 @@ func BlacklistDelete(id string) error {
 		"id":     id,
 		"prefix": meta.Prefix,
 	}
+
 	data := Blacklist{}
 	query, _, _ := dialect.From("tbl_blacklist").Select(colsBlacklist...).Where(ex).ToSQL()
 	err := meta.MerchantDB.Get(&data, query)
@@ -156,111 +136,25 @@ func BlacklistDelete(id string) error {
 		return pushLog(fmt.Errorf("%s,[%s]", err.Error(), query), helper.DBErr)
 	}
 
-	tx, err := meta.MerchantDB.Begin()
-	if err != nil {
-		return pushLog(err, helper.DBErr)
-	}
-
 	query, _, _ = dialect.Delete("tbl_blacklist").Where(ex).ToSQL()
-	_, err = tx.Exec(query)
+	_, err = meta.MerchantDB.Exec(query)
 	if err != nil {
 		return pushLog(fmt.Errorf("%s,[%s]", err.Error(), query), helper.DBErr)
 	}
 
-	idx := MurmurHash(data.Value, 0) % 10
-	key := fmt.Sprintf("bl:dev%d", idx)
+	key := ""
 	switch data.Ty {
-	case TyDevice:
 	case TyIP:
-		key = fmt.Sprintf("bl:ip%d", idx)
-	case TyEmail:
-		key = fmt.Sprintf("bl:em%d", idx)
+		key = "ip_blacklist"
 	case TyPhone:
-		key = fmt.Sprintf("bl:ph%d", idx)
+		key = "phone_blacklist"
 	case TyBankcard:
-		bc, err := BankCardFindOne(ex)
-		if err != nil {
-			return err
-		}
-
-		// 不是删除也不是冻结
-		if bc.State != 2 && bc.State != 3 {
-			return errors.New(helper.OperateFailed)
-		}
-
-		// 冻结状态直接恢复
-		if bc.State == 3 {
-			query, _, _ = dialect.Update("tbl_member_bankcard").Set(g.Record{"state": 1}).Where(g.Ex{"id": id, "prefix": meta.Prefix}).ToSQL()
-			_, err = tx.Exec(query)
-			if err != nil {
-				_ = tx.Rollback()
-				return pushLog(fmt.Errorf("%s,[%s]", err.Error(), query), helper.DBErr)
-			}
-		}
-
-		hash, _ := strconv.ParseUint(data.Value, 10, 64)
-		idx = hash % 10
-		key = fmt.Sprintf("bl:bc%d", idx)
-	case TyVirtualAccount:
-		key = fmt.Sprintf("bl:va%d", idx)
-	default:
+		key = "bankcard_blacklist"
 	}
 
-	err = tx.Commit()
-	if err != nil {
-		return pushLog(err, helper.DBErr)
-	}
-
-	// 删除value set
-	_, err = meta.MerchantRedis.SRem(ctx, key, data.Value).Result()
-	if err != nil {
-		return pushLog(err, helper.RedisErr)
-	}
+	meta.MerchantRedis.Do(ctx, "CF.DEL", key, data.Value).Err()
 
 	return nil
-}
-
-// 获取黑名单会员
-/*
-ty 1 设备号 2 ip
-value 设备号或者ip的值
-*/
-func BlacklistFindUsers(ty int, value string) []string {
-
-	if ty != TyDevice && ty != TyIP {
-		return nil
-	}
-
-	key := "device_no.keyword"
-	if ty == TyIP {
-		key = "ips.keyword"
-	}
-	query := elastic.NewBoolQuery().Must(elastic.NewTermQuery(key, value), elastic.NewTermQuery("prefix", meta.Prefix))
-	agg := elastic.NewTermsAggregation().Field("username.keyword").Size(10000).
-		SubAggregation("username", elastic.NewCardinalityAggregation().Field("username.keyword"))
-	resOrder, err := meta.ES.Search().Index(esPrefixIndex("memberlogin")).
-		Query(query).Size(0).Aggregation("group", agg).Do(ctx)
-	if err != nil {
-		return nil
-	}
-
-	terms, ok := resOrder.Aggregations.Terms("group")
-	if !ok {
-		return nil
-	}
-
-	var data []string
-	for _, v := range terms.Buckets {
-
-		key, ok := v.Key.(string)
-		if !ok {
-			continue
-		}
-
-		data = append(data, key)
-	}
-
-	return data
 }
 
 // 满足条件的黑名单数量
@@ -276,51 +170,31 @@ func BlacklistExist(ex g.Ex) bool {
 
 func BlacklistLoadCache() error {
 
-	t := dialect.From("tbl_blacklist")
-	for i := TyDevice; i <= TyVirtualAccount; i++ {
-
-		keyFmt := "bl:dev%d"
-		switch i {
-		case TyDevice:
-		case TyIP:
-			keyFmt = "bl:ip%d"
-		case TyEmail:
-			keyFmt = "bl:em%d"
-		case TyPhone:
-			keyFmt = "bl:ph%d"
-		case TyBankcard:
-			keyFmt = "bl:bc%d"
-		case TyVirtualAccount:
-			keyFmt = "bl:va%d"
-		default:
-		}
-
-		var values []string
-		ex := g.Ex{
-			"ty":     i,
-			"prefix": meta.Prefix,
-		}
-		query, _, _ := t.Select("value").Where(ex).ToSQL()
-		err := meta.MerchantDB.Select(&values, query)
-		if err != nil {
-			return pushLog(fmt.Errorf("%s,[%s]", err.Error(), query), helper.DBErr)
-		}
-
-		pipe := meta.MerchantRedis.TxPipeline()
-		for _, v := range values {
-			idx := MurmurHash(v, 0) % 10
-			if i == TyBankcard {
-				hash, _ := strconv.ParseUint(v, 10, 64)
-				idx = hash % 10
-			}
-			key := fmt.Sprintf(keyFmt, idx)
-			pipe.SAdd(ctx, key, v)
-		}
-		_, err = pipe.Exec(ctx)
-		if err != nil {
-			return pushLog(err, helper.RedisErr)
-		}
+	data := []Blacklist{}
+	query, _, _ := dialect.From("tbl_blacklist").Select(colsBlacklist...).ToSQL()
+	err := meta.MerchantDB.Select(&data, query)
+	if err != nil {
+		return err
 	}
+
+	pipe := meta.MerchantRedis.Pipeline()
+	pipe.Unlink(ctx, "phone_blacklist", "bankcard_blacklist", "ip_blacklist")
+	for _, v := range data {
+		key := ""
+		switch v.Ty {
+		case TyIP:
+			key = "ip_blacklist"
+		case TyPhone:
+			key = "phone_blacklist"
+		case TyBankcard:
+			key = "bankcard_blacklist"
+		}
+
+		pipe.Do(ctx, "CF.ADD", key, v.Value)
+	}
+
+	pipe.Exec(ctx)
+	pipe.Close()
 
 	return nil
 }
