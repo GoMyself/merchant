790fbe8d08be9d9b64ac36715992d5b1aeccd44f
mige <nonn35775@gmail.com>
2022-05-17 20:30:15 +0800

    按代理查询

--- controller/sms.go
+++ controller/sms.go
@@ -1,1 +1,21 @@
 package controller
+
+//import (
+//	"github.com/valyala/fasthttp"
+//	"merchant2/contrib/helper"
+//)
+//
+//// SMSController 会员端接口
+//type SMSController struct{}
+//
+//// List 活动列表及按 flag 筛选
+//func (*SMSController) List(ctx *fasthttp.RequestCtx) {
+//
+//	s, err := model.PromoList()
+//	if err != nil {
+//		helper.Print(ctx, false, err.Error())
+//		return
+//	}
+//
+//	helper.PrintJson(ctx, true, s)
+//}
