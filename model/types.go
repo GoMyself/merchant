1fcf67acddcef6ab0656819e6e21eb51d1acf2c7
mige <nonn35775@gmail.com>
2022-05-17 16:41:54 +0800

    替换 风控管理/登陆日志 查询 TD

--- model/types.go
+++ model/types.go
@@ -156,23 +156,30 @@ type MemberLoginLogData struct {
 	T int64            `json:"t"`
 }
 
+//type MemberLoginLog struct {
+//	Username string `msg:"username" json:"username"`
+//	IP       int64  `msg:"ip" json:"ip"`
+//	IPS      string `msg:"ips" json:"ips"`
+//	Device   string `msg:"device" json:"device"`
+//	DeviceNo string `msg:"device_no" json:"device_no"`
+//	Date     uint32 `msg:"date" json:"date"`
+//	Serial   string `msg:"serial" json:"serial"`
+//	Agency   bool   `msg:"agency" json:"agency"`
+//	Parents  string `msg:"parents" json:"parents"`
+//	IsRisk   int    `msg:"-" json:"is_risk"`
+//}
+
 type MemberLoginLog struct {
-	Username string `db:"username" json:"username"`
-	IP       string `db:"ip" json:"ip"`
-	//IPS      string `msg:"ips" json:"ips"`
-	Device     string `db:"device" json:"device"`
+	Username   string `db:"username" json:"username"`
+	IP         string `db:"ip" json:"ip"`
+	Device     int    `db:"device" json:"device"`
 	DeviceNo   string `db:"device_no" json:"device_no"`
 	TopUID     string `db:"top_uid" json:"top_uid"`         // 总代uid
 	TopName    string `db:"top_name" json:"top_name"`       // 总代代理
 	ParentUID  string `db:"parent_uid" json:"parent_uid"`   // 上级uid
 	ParentName string `db:"parent_name" json:"parent_name"` // 上级代理
-	//Date     uint32 `msg:"date" json:"date"`
-	//Serial   string `msg:"serial" json:"serial"`
-	//Agency   bool   `msg:"agency" json:"agency"`
-	//Parents string `msg:"parents" json:"parents"`
-	//IsRisk   int    `msg:"-" json:"is_risk"`
-	CreateAt string `db:"create_at" json:"create_at"`
-	Prefix   string `db:"prefix" json:"prefix"`
+	CreateAt   string `db:"create_at" json:"create_at"`
+	//Prefix   string `db:"prefix" json:"prefix"`
 }
 
 type memberRemarkLogData struct {
