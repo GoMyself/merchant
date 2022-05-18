baf9bb3ccd511065cfe6ae8d6f492ec7fe6c993a
mige <nonn35775@gmail.com>
2022-05-18 20:55:43 +0800

    短信通道新建及更新原型调整

--- router/router.go
+++ router/router.go
@@ -327,7 +327,7 @@ func SetupRouter(b BuildInfo) *fasthttprouter.Router {
 	// 运营管理-短信通道-列表
 	post("/merchant/sms/list", smsChannelCtl.List)
 	// 运营管理-短信通道-增加
-	//post("/merchant/sms/insert", smsChannelCtl.Insert)
+	post("/merchant/sms/insert", smsChannelCtl.Insert)
 	// 运营管理-短信通道-编辑
 	post("/merchant/sms/update", smsChannelCtl.Update)
 	// 运营管理-短信通道-更新短信通道状态
