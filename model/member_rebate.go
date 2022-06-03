409f9923ebd382e1f0bf285402e884b51b5a1f76
Linch <Linch@gmail.com>
2022-06-03 14:17:07 +0800

    xg

--- model/member_rebate.go
+++ model/member_rebate.go
@@ -52,7 +52,7 @@ func MemberRebateCmp(uid string, own MemberRebateResult_t) bool {
 func MemberRebateUpdateCache1(uid string, mr MemberRebateResult_t) error {
 
 	key := fmt.Sprintf("%s:m:rebate:%s", meta.Prefix, uid)
-	vals := []interface{}{"zr", mr.ZR.Truncate(1), "qp", mr.QP.Truncate(1), "ty", mr.TY.Truncate(1), "dj", mr.DJ.Truncate(1), "dz", mr.DZ.Truncate(1), "cp", mr.CP.Truncate(1), "fc", mr.FC.Truncate(1), "by", mr.BY.Truncate(1), "cg_high_rebate", mr.CGHighRebate.Truncate(2), "cg_official_rebate", mr.CGOfficialRebate.Truncate(2)}
+	vals := []interface{}{"zr", mr.ZR.StringFixed(1), "qp", mr.QP.StringFixed(1), "ty", mr.TY.StringFixed(1), "dj", mr.DJ.StringFixed(1), "dz", mr.DZ.StringFixed(1), "cp", mr.CP.StringFixed(1), "fc", mr.FC.StringFixed(1), "by", mr.BY.Truncate(1), "cg_high_rebate", mr.CGHighRebate.StringFixed(2), "cg_official_rebate", mr.CGOfficialRebate.StringFixed(2)}
 
 	pipe := meta.MerchantRedis.Pipeline()
 	pipe.Del(ctx, key)
