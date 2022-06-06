6aa28b88719eceb6edec331d94606dfd67076019
brandon <brandon@gmail.com>
2022-06-06 16:38:39 +0700

    fix

--- model/group.go
+++ model/group.go
@@ -187,7 +187,7 @@ func GroupList() (string, error) {
 
 func GroupClosureInsert(nodeID, targetID string) string {
 
-	t := "SELECT ancestor, " + nodeID + ",prefix, lvl+1 FROM tbl_members_tree WHERE prefix='" + meta.Prefix + "' and descendant = " + targetID + " UNION SELECT " + nodeID + "," + nodeID + "," + "'" + meta.Prefix + "'" + ",0"
+	t := "SELECT ancestor, " + nodeID + ",prefix, lvl+1 FROM tbl_admin_group_tree WHERE prefix='" + meta.Prefix + "' and descendant = " + targetID + " UNION SELECT " + nodeID + "," + nodeID + "," + "'" + meta.Prefix + "'" + ",0"
 	query := "INSERT INTO tbl_admin_group_tree (ancestor, descendant,prefix,lvl) (" + t + ")"
 
 	return query
