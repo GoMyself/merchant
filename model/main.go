f44d17f2f5cf55e2db74e47b0e43698dcc848aee
brandon <brandon@gmail.com>
2022-06-07 21:36:20 +0700

    fix

--- model/main.go
+++ model/main.go
@@ -200,7 +200,7 @@ func pushLog(err error, code string) error {
 		"project":  meta.Program,
 		"flags":    code,
 		"filename": path,
-		"ts":       ts.In(loc).UnixMilli(),
+		"ts":       ts.In(loc).UnixMicro(),
 	}
 	query, _, _ := dialect.Insert("goerror").Rows(&fields).ToSQL()
 	fmt.Println("insert SMS = sql ", query)
