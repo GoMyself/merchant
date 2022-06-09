632a56f691dd14547649d3f3e4d13072a2105338
brandon <brandon@gmail.com>
2022-06-09 14:01:46 +0700

    fix

--- model/group.go
+++ model/group.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	g "github.com/doug-martin/goqu/v9"
 	"merchant/contrib/helper"
+	"sort"
 	"strings"
 	"time"
 )
@@ -89,7 +90,9 @@ func GroupUpdate(gid, adminGid string, data Group) error {
 		fmt.Println(subs)
 		for _, v := range subs {
 			privs := ""
-			for _, vv := range strings.Split(v.Permission, ",") {
+			pms := strings.Split(v.Permission, ",")
+			sort.Strings(pms)
+			for _, vv := range pms {
 				// 下级权限在分组权限调整后的范围内保留，不在则删除
 				if _, ok = gPrivMap[vv]; ok {
 					if privs != "" {
