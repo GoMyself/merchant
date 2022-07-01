74966786a332ca35162dacc9f1298a3f1e2187f6
brandon <brandon@gmail.com>
2022-07-01 22:45:23 +0700

    mod

--- service.go
+++ service.go
@@ -10,7 +10,7 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func telegramBotNotice(program, gitReversion, buildTime, buildGoVersion, flag string) {
+func telegramBotNotice(program, gitReversion, buildTime, buildGoVersion, flag, prefix string) {
 
 	var localIp string
 	ts := time.Now()
@@ -20,7 +20,7 @@ func telegramBotNotice(program, gitReversion, buildTime, buildGoVersion, flag st
 	}
 
 	bot.Debug = false
-	str := "\U00002733	\U00002733	\U00002733\r\ndatetime: \t%s\r\nprogram: \t%s\r\nGitReversion: \t%s\r\nBuildTime: \t%s\r\nBuildGoVersion: \t%s\r\nhostname: \t%s\r\nIP: \t%s\r\nflag: \t%s\n\U00002728 \U00002728 \U00002728\r\n"
+	str := "‼️‼️生产环境发版‼️‼️\r\n✳️ ✳️ ✳️\r\n⚠️datetime: \t%s\r\n⚠️program: \t%s\r\n⚠️GitReversion: \t%s\r\n⚠️BuildTime: \t%s\r\n⚠️BuildGoVersion: \t%s\r\n⚠️hostname: \t%s\r\n⚠️IP: \t%s\r\n⚠️flag: \t%s\r\n⚠️prefix: \t%s\n✨ ✨ ✨\r\n"
 	hostname, err := os.Hostname()
 	if err != nil {
 		log.Fatal(err)
@@ -43,7 +43,7 @@ func telegramBotNotice(program, gitReversion, buildTime, buildGoVersion, flag st
 	}
 
 	msg := tgbotapi.NewMessage(-738052985, "")
-	msg.Text = fmt.Sprintf(str, ts.Format("2006-01-02 15:04:05"), program, gitReversion, buildTime, buildGoVersion, hostname, localIp, flag)
+	msg.Text = fmt.Sprintf(str, ts.Format("2006-01-02 15:04:05"), program, gitReversion, buildTime, buildGoVersion, hostname, localIp, flag, prefix)
 	if _, err := bot.Send(msg); err != nil {
 		fmt.Println("tgbot error : ", err.Error())
 	}
